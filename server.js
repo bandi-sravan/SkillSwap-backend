e453189006e1c54abc25fc3894b665902b33baf3
bandi-sravan <sravanbandi2006@gmail.com>
2026-03-15 11:11:31 +0530

    Added MongoDB Atlas connection for skillswap backend

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./db.js');
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 app.use(cors());
